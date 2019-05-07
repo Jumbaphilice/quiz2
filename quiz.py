d9a517a7416a62303361b6a6c604c7a5949fbf79
Mercymuhonja <muhonjamercy63@gmail.com>
2019-05-07 19:20:30 +0300

    quiz2

---
+++ quiz.py
@@ -0,0 +1,55 @@
+def Tens():
+	list[1,2,3,4,5,6,7,8,9]
+	for x in list:
+		n=x*10
+		print(n)
+
+def Square():
+	list[2,4,6,8,10,12]
+	for s in list:
+		t=s**2
+		print(t)
+
+def sorted():
+	a=[9,1,4,7,3]
+	b=[5,2,6,8,0]
+	sorted_list=[]
+	if (a[0] <= b[0]): 
+            x = a.(0) 
+            sorted_list.append(x)
+        else:
+            x= b.(0)
+            sorted_list.append(x)
+
+    sorted_list.extend(a if a else b)
+
+    return sorted_list
+
+  def range_sum(n,n+1):
+  	var sum = 0;
+    for(var i = 1; i <= N; i++)
+    return sum
+
+  def largest():
+  	list=[1,3,6,8,2,4,5.7]
+  	print(list(8))
+
+  def smallest():
+  	min=[1,3,6,8,2,4,5.7]
+  	print(min(1))
+
+  def my_modulus():
+  	f=dict()
+  	for p in range (10,20);
+  		d[p]%3
+  		print(d)
+
+  def students():
+  	
+
+
+     
+
+
+
+
